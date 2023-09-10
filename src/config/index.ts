38050f2b64e3fec8392a8e20fc7dff7a5841babf
Kevin Vega <kevinvega2070@gmail.com>
2023-09-10 19:03:19 -0300

    fix: unused client removed

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,2 @@
 export { default as horusAPI } from './horusAPI';
 export { default as openWeather25API } from './openWeather25API';
-export { default as openWeatherTileAPI } from './openWeatherTileAPI';
