0565ca47db48be6cbc39587ae330d516e8a61149
Kevin Vega <kevinvega2070@gmail.com>
2023-09-10 19:06:59 -0300

    feat: new component added (WeatherIcon)

---
+++ src/components/ui/WeatherIcon/index.tsx
@@ -0,0 +1,60 @@
+import { type FC } from 'react';
+
+import AcUnitOutlinedIcon from '@mui/icons-material/AcUnitOutlined';
+import BlockOutlinedIcon from '@mui/icons-material/BlockOutlined';
+import CloudOutlinedIcon from '@mui/icons-material/CloudOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import NightsStayOutlinedIcon from '@mui/icons-material/NightsStayOutlined';
+import ThunderstormOutlinedIcon from '@mui/icons-material/ThunderstormOutlined';
+import WaterDropOutlinedIcon from '@mui/icons-material/WaterDropOutlined';
+import WavesOutlinedIcon from '@mui/icons-material/WavesOutlined';
+
+import type * as T from './types';
+
+const WeatherIcon: FC<T.Props> = ({ iconId }) => {
+
+  switch (iconId) {
+
+    case '01d':
+      return <LightModeOutlinedIcon/>;
+
+    case '01n':
+      return <DarkModeOutlinedIcon/>;
+
+    case '02d':
+    case '03d':
+    case '03n':
+    case '04d':
+    case '04n':
+      return <CloudOutlinedIcon/>;
+
+    case '02n':
+      return <NightsStayOutlinedIcon/>;
+
+    case '09d':
+    case '09n':
+    case '10d':
+    case '10n':
+      return <WaterDropOutlinedIcon/>;
+
+    case '11d':
+    case '11n':
+      return <ThunderstormOutlinedIcon/>;
+
+    case '13d':
+    case '13n':
+      return <AcUnitOutlinedIcon/>;
+
+    case '50d':
+    case '50n':
+      return <WavesOutlinedIcon/>;
+
+    default:
+      return <BlockOutlinedIcon/>;
+
+  };
+
+};
+
+export default WeatherIcon;
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Card } from './Card';
 export { default as Map } from './Map';
 export { default as ProgressBar } from './ProgressBar';
+export { default as WeatherIcon } from './WeatherIcon';
